707bcf6ca32f90d71a5c67783edfa3f04034c936
unknown <richarddowsett@gmail.com>
2016-01-17 18:35:06 +0000

    remove progressbar

--- js/timer.js
+++ js/timer.js
@@ -2,29 +2,21 @@
 
 	console.log("Hello, I'm a timer")
 
-   var circle = new ProgressBar.Circle('#progressBar', {
-        strokeWidth: 3,
-        trailWidth: 1,
-        text: {
-            value: "00:00"
-        },
-
-    });
-    console.log("I have just created " + circle)
+    // console.log("I have just created " + circle)
 
 var timeInterval = null
 
 function startTimerObj(configuration, i) {
-    circle.set(0)
+    // circle.set(0)
 	console.log(configuration.length + " -> " + i)
-	console.log(circle)
-	circle.setText("Working")
+	// console.log(circle)
+	// circle.setText("Working")
 	var first = configuration[i]
 	var minutes = first.minutes
 	var seconds = first.seconds
 	var duration = parseInt(minutes) * 60 + parseInt(seconds)
     console.log("Duration -> " + duration)
-    circle.duration = duration
+    // circle.duration = duration
     var totalDuration = duration, minutes, seconds;
     var originalDuration = totalDuration
     var durationCount = 1
@@ -39,18 +31,18 @@ function startTimerObj(configuration, i) {
         secondString = seconds < 10 ? "0" + seconds : seconds;
         console.log(minuteString + ":" + secondString)
         console.log("Animate to " + durationCount + "/" + originalDuration + ". totalDuration = " + totalDuration)
-        circle.animate(durationCount / originalDuration, {
-            duration : 1000,
-            from : {color : circleColour},
-            to : {color : circleColour}
-        })
+        // circle.animate(durationCount / originalDuration, {
+        //     duration : 1000,
+        //     from : {color : circleColour},
+        //     to : {color : circleColour}
+        // })
 
-        circle.setText(minuteString + ":" + secondString);
+        // circle.setText(minuteString + ":" + secondString);
         --totalDuration
         console.log("Looping")
         if (++durationCount == originalDuration + 2) {
             clearInterval(timeInterval)
-            circle.stop()
+            // circle.stop()
             console.log(configuration.length + " -> " + i)
             if(i < configuration.length - 1){
             	startTimerObj(configuration, ++i)
